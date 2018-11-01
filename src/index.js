4c7ad8c37e7872e364d6a9dce3722439ff7d9a04
yoavniran <yoavniran@gmail.com>
2018-11-01 14:55:53 +0200

    remove unneeded code from create-react-app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import getStore from "./store/store";
 import App from "./components/App/App";
-// import * as serviceWorker from './serviceWorker';
 import "./index.scss";
-
 
 const store = getStore();
 
@@ -16,9 +14,3 @@ ReactDOM.render(
 		<App />
 	</Provider>,
 	document.getElementById("root"));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
